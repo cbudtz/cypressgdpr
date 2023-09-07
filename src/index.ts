1608e6fffe10622d132dffea0f116bc0b8d26ef4
Christian <chrbudtz@gmail.com>
2023-09-07 17:00:38 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ const app = express();
 
 app.get('/facebook', async (req: any, res: any) => {
     const browser = await puppeteer.launch({
-        headless: false
+        headless: false,
+        args: [ "--disable-notifications" ]
     }
     );
     const page = await browser.newPage();
@@ -28,7 +29,29 @@ app.get('/facebook', async (req: any, res: any) => {
             }
         });
     }, "Log In");
-
+    await page.waitForNavigation();
+    await page.goto("https://www.facebook.com/dyi/?referrer=yfi_settings")
+    await page.evaluate((text) => {
+        const spans = document.querySelectorAll('span');
+        spans.forEach((span) => {
+            if (span.textContent === text) {
+                span.click();
+            }
+        });
+    }, "Anmod om download");
+    console.log("Looking for Komplet kopi span")
+    
+    await page.evaluate((text) => {
+        const spans = document.querySelectorAll('span');
+        console.log("found spans" + spans.length)
+        spans.forEach((span) => {
+            if (span.textContent?.includes(text)) {
+                span.click();
+                console.log("found button")
+            }
+        });
+    }, "Komplet kopi");
+    console.log("Not a lot of luck")
     res.send(await page.content());
 
 
